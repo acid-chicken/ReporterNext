a3c023cb7b7313b274c11cfa42d4aaab834f47ca
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-08-08 01:42:50 +0900

    Beauty code

--- ReporterNext/Controllers/WebhooksController.cs
+++ ReporterNext/Controllers/WebhooksController.cs
@@ -42,56 +42,16 @@ namespace ReporterNext.Controllers
             {
                 switch (@event)
                 {
-                    case TweetCreateEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<TweetCreateEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case FavoriteEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<FavoriteEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case FollowEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<FollowEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case BlockEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<BlockEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case MuteEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<MuteEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case UserRevokeEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<UserRevokeEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case DirectMessageEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<DirectMessageEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case DirectMessageIndicateTypingEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<DirectMessageIndicateTypingEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case DirectMessageMarkReadEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<DirectMessageMarkReadEvent>(forUserId)
-                            .Execute(x));
-                        break;
-                    case TweetDeleteEvent x:
-                        BackgroundJob.Enqueue(() => factory
-                            .Create<TweetDeleteEvent>(forUserId)
-                            .Execute(x));
-                        break;
+                    case TweetCreateEvent x: BackgroundJob.Enqueue(() => factory.Create<TweetCreateEvent>(forUserId).Execute(x)); break;
+                    case FavoriteEvent x: BackgroundJob.Enqueue(() => factory.Create<FavoriteEvent>(forUserId).Execute(x)); break;
+                    case FollowEvent x: BackgroundJob.Enqueue(() => factory.Create<FollowEvent>(forUserId).Execute(x)); break;
+                    case BlockEvent x: BackgroundJob.Enqueue(() => factory.Create<BlockEvent>(forUserId).Execute(x)); break;
+                    case MuteEvent x: BackgroundJob.Enqueue(() => factory.Create<MuteEvent>(forUserId).Execute(x)); break;
+                    case UserRevokeEvent x: BackgroundJob.Enqueue(() => factory.Create<UserRevokeEvent>(forUserId).Execute(x)); break;
+                    case DirectMessageEvent x: BackgroundJob.Enqueue(() => factory.Create<DirectMessageEvent>(forUserId).Execute(x)); break;
+                    case DirectMessageIndicateTypingEvent x: BackgroundJob.Enqueue(() => factory.Create<DirectMessageIndicateTypingEvent>(forUserId).Execute(x)); break;
+                    case DirectMessageMarkReadEvent x: BackgroundJob.Enqueue(() => factory.Create<DirectMessageMarkReadEvent>(forUserId).Execute(x)); break;
+                    case TweetDeleteEvent x: BackgroundJob.Enqueue(() => factory.Create<TweetDeleteEvent>(forUserId).Execute(x)); break;
                 }
             }
             return Ok();
