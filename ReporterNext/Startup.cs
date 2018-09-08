1322f77c8d13049a3cc9bc65d8e26887f060e598
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-09-08 18:06:05 +0900

    Fix bug

--- ReporterNext/Startup.cs
+++ ReporterNext/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using ReporterNext.Components;
 using ReporterNext.Models;
 using StackExchange.Redis;
