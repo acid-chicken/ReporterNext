890cd2033b4beb47b18fe0748f02ad21d86bbbb9
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-09-10 03:17:20 +0900

    Refactoring

--- ReporterNext/Components/ReactiveObservers.cs
+++ ReporterNext/Components/ReactiveObservers.cs
@@ -41,23 +41,20 @@ namespace ReporterNext.Components
         public static Task JobAsync(string consumerKey, string consumerSecret, string accessToken, string accessTokenSecret, TweetCreateEvent @event)
         {
             var tokens = Tokens.Create(consumerKey, consumerSecret, accessToken, accessTokenSecret);
-            return
-                (@event.Target.QuotedStatusId ?? @event.Target.QuotedStatus?.Id) is long quotedId &&
-                    @event.Target.User.Id is long userId ?
+            Task ReplyAsync(long id) =>
                     tokens.Statuses.UpdateAsync(
-                        status => $"ツイート時刻：{quotedId.ToSnowflake().ToOffset(new TimeSpan(9, 0, 0)):HH:mm:ss.fff}",
+                    status => $"ツイート時刻：{id.ToSnowflake().ToOffset(new TimeSpan(9, 0, 0)):HH:mm:ss.fff}",
                         in_reply_to_status_id => @event.Target.Id,
                         auto_populate_reply_metadata => true,
                         include_ext_alt_text => true,
-                        tweet_mode => TweetMode.Extended) :
+                    tweet_mode => TweetMode.Extended);
+            return
+                (@event.Target.QuotedStatusId ?? @event.Target.QuotedStatus?.Id) is long quotedId &&
+                    @event.Target.User.Id is long userId ?
+                    ReplyAsync(quotedId) :
                 @event.Target.InReplyToStatusId is long replyId &&
                 @event.Target.User.Id.ToString() != accessToken.Split('-')[0] ?
-                    tokens.Statuses.UpdateAsync(
-                        status => $"ツイート時刻：{replyId.ToSnowflake().ToOffset(new TimeSpan(9, 0, 0)):HH:mm:ss.fff}",
-                        in_reply_to_status_id => @event.Target.Id,
-                    auto_populate_reply_metadata => true,
-                        include_ext_alt_text => true,
-                    tweet_mode => TweetMode.Extended) :
+                    ReplyAsync(replyId) :
                 Task.CompletedTask;
     }
     }
