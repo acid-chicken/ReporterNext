bec8e42f2ece2b6f19448da886f552338a7b800a
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2020-05-27 08:51:37 +0900

    Fix bug

--- ReporterNext/Components/ReactiveObservers.cs
+++ ReporterNext/Components/ReactiveObservers.cs
@@ -190,10 +190,13 @@ namespace ReporterNext.Components
                         .ContinueWith(x =>
                         {
                             var status = x.Result.FirstOrDefault(x => targets.IsMatch(x.FullText ?? x.Text));
+                            var text = status is null ?
+                                "エラー：ツイートが見つかりませんでした。" :
+                                $"ツイート時刻：{status.Id.ToSnowflake().ToOffset(new TimeSpan(9, 0, 0)):HH:mm:ss.fff} https://twitter.com/{status.User.ScreenName}/status/{status.Id}";
 
                             return tokens.DirectMessages.Events.NewAsync(
                                 recipient_id => recipientId,
-                                text => $"ツイート時刻：{status.Id.ToSnowflake().ToOffset(new TimeSpan(9, 0, 0)):HH:mm:ss.fff} https://twitter.com/{status.User.ScreenName}/status/{status.Id}");
+                                text => text);
                         }));
 
             return metadataSections[0] switch
